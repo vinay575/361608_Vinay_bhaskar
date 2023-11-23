872f713912f7b36da012a3dac7abed0bddc8b16d
vinay <bhaskarvinaychitturi@gmail.com>
2023-11-23 22:17:52 +0530

    updte

---
+++ full_stack/java/wek/type.java
@@ -0,0 +1,11 @@
+class type {
+    public static void main(String[] args) {
+     byte b = 127;
+     int a =b;
+     byte c = (byte)a;  //type casting  select what type you want and keep it in brackets
+
+     float k = 12.03f;
+     int f = (int)k;      // float to int
+     System.out.println(f);
+    }
+}
