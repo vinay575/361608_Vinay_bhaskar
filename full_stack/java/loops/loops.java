ac6105f7ee0f2b256523dd084f0e3344ef1e52ef
vinay <bhaskarvinaychitturi@gmail.com>
2023-11-25 15:03:34 +0530

    message

--- full_stack/java/loops/loops.java
+++ full_stack/java/loops/loops.java
@@ -37,3 +37,13 @@ class Switch3{
         while(i<=3);
 }
 }
+  class loop1{
+    public static void main (String [] args){
+
+        for (int i=1;i<=7;i++){
+            System.out.println("Day"+i);
+            for (int j=1;j<=4;j++){
+                System.out.println("hi"+(j+1));
+            }
+        }
+    }}
